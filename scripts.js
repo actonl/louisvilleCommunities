bb82efe473db8bfc62d5b2d60cf87da6f4cf4869
unknown <43154375+actonl@users.noreply.github.com>
2018-11-14 17:06:09 -0500

    add img files, update html and css, add js function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,21 @@
 
+// navbar scroll color change
 $(window).scroll(function(){
 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 700);
 });
+
+// image gallery collapsible
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
